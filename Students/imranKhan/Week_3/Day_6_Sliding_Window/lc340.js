48710de82e4f6cbeda7aae7331fab933090324fd
Imran Khan <imran.khan2373@gmail.com>
2022-06-14 01:37:12 -0400

    Attempted lc209 without objects but it doesn't seem to be working correctly

--- Students/imranKhan/Week_3/Day_6_Sliding_Window/lc340.js
+++ Students/imranKhan/Week_3/Day_6_Sliding_Window/lc340.js
@@ -5,7 +5,32 @@
 // You can assume that K is less than or equal to the length of the given string.
 
 function longest_substring_with_k_distinct(str, k) {
-  // TODO: Write code here
+  let windowStart = 0;
+  let substring = "";
+  let maxLength = 0;
+  let length = 0;
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++)
+  {
+    if (length > maxLength)
+      maxLength = length;
+
+    console.log(typeof(str[windowEnd]));
+    if (!substring.includes('3'))
+    {
+      substring += str[windowEnd];
+      length++;
+    } else
+    {
+      substring -= str[windowStart];
+      windowStart++;
+    }
+
+    if (length > k)
+      length--;
+  }
+
+  return maxLength;
 }
 
 console.log(
